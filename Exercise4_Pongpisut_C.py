f8696a17d4cba491fcb6341870600b8b5e8cb999
pongpisut261 <new.pongpisut@gmail.com>
2020-04-28 22:31:18 +0700

    Exercise4_Pongpisut_C.py

---
+++ Exercise4_Pongpisut_C.py
@@ -0,0 +1,9 @@
+print('--- Your Score ---')
+Foundation_English = 80.5
+General_Business = 75.0
+Introduction_to_Computer_Systems = 89.0
+Computer_Programmimg = 88.5
+print('Foundation English :' , Foundation_English)
+print('General Business :' , General_Business)
+print('Introduction to Computer Systems :' , Introduction_to_Computer_Systems)
+print('Computer Programmimg : ' , Computer_Programmimg)
