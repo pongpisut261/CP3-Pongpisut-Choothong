d5ec3a5566f1203547b6175cea8333b2c5acb1e6
pongpisut261 <new.pongpisut@gmail.com>
2020-05-01 21:51:57 +0700

    Lecture50_Pongpisut_C

---
+++ Lecture50_Pongpisut_C.py
@@ -0,0 +1,12 @@
+def number1(num1,num2):
+    print(num1 + num2)
+number1(100,50)
+def number2(num1,num2):
+    print(num1 - num2)
+number2(100,50)
+def number3(num1,num2):
+    print(num1 * num2)
+number3(100,50)
+def number4(num1,num2):
+    print(num1 / num2)
+number4(100,50)
